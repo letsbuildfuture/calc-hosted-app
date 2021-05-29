2084b7396f57f82af0621e452c05c3c01fb90e0f
saurabh1991singh <saursbh1991singh@gmail.com>
2021-05-29 19:13:44 +0530

    [Commit 03] Time Calculation added.

--- src/app/MyComponent/output-block/output-block.component.ts
+++ src/app/MyComponent/output-block/output-block.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'app-output-block',
@@ -7,10 +7,50 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class OutputBlockComponent implements OnInit {
   @Input() output: any;
+  @Input() startDate: any;
+  @Input() endDate: any;
+  outputHrs?: any;
+  outputWeeks: any;
+  outputMonths: any;
+  outputYears: any;
   constructor() { }
 
   ngOnInit(): void {
-    this.output = Math.abs(this.output);
+    this.calculateDateTime(this.output);
+  }
+  ngOnChanges() {
+    this.calculateDateTime(this.output);
+  }
+  calculateDateTime(outputDays: any){
+    let days=outputDays;
+    let startDate=new Date(this.startDate);
+    let endDate=new Date(this.endDate);
+    let years: any;
+    let yearsAndMonth: any;
+    let yearsAndMonthAndDays: any;
+    this.output = Math.abs(days);
+    this.outputHrs = days>0? "or, " + 24*days + " Hours" : "";
+
+    
+
+    let months = (startDate.getFullYear() - endDate.getFullYear()) * 12;
+    months -= startDate.getMonth();
+    months += endDate.getMonth();
+    months = months <= 0 ? 0 : months;
+    let monthsAndDays = Math.abs(startDate.getDate() - endDate.getDate());
+    this.outputMonths = months>0? monthsAndDays>0 ? "or,  " + months + " Months and "+monthsAndDays + " Days" : "or,  " + months+" Months" : "";
+
+    let weeks = Math.floor(days/7);
+    let weekAndDays = Math.floor(days%7);
+    this.outputWeeks = weeks > 0 ? weekAndDays>0 ? "or,  " + weeks + " Weeks and "+weekAndDays+" Days" : "or/ " + weeks + " Weeks" : "";
+
+    years = Math.abs(startDate.getFullYear() - endDate.getFullYear());
+    years = years > 0 ? "or, " + years + " Years" : "";
+    yearsAndMonth = Math.abs(startDate.getMonth() - endDate.getMonth());
+    yearsAndMonth = yearsAndMonth > 0 ? " and " + yearsAndMonth + " Months" : "";
+    yearsAndMonthAndDays = Math.abs(startDate.getDate() - endDate.getDate());
+    yearsAndMonthAndDays = yearsAndMonthAndDays > 0 ? " and " + yearsAndMonthAndDays + " Days" : "";
+    this.outputYears = years + yearsAndMonth + yearsAndMonthAndDays ;
   }
 
 }
